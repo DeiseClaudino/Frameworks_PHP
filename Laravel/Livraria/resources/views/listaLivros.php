8a1e8bd996bf5e457fd7433ff7b79bdf14485b50
DeiseClaudino <deise.claudino98@gmail.com>
2018-08-01 22:59:25 -0300

    Melhorando a aparencia da listagem através da estiização com Bootstrap.

--- Laravel/Livraria/resources/views/listaLivros.php
+++ Laravel/Livraria/resources/views/listaLivros.php
@@ -1,30 +1,36 @@
 <html>
+  <head>
+      <link href="/css/app.css" rel="stylesheet">
+      <title>Controle de estoque</title>
+  </head>
   <body>
-  <h1>Listagem de Livros</h1>
-    <table>
-      
-      <tr>
-        <th>nome</th>
-        <th>preco</th>
-        <th>descricao</th>
-        <th>isbn</th>
-        <th>tipoLivro</th>
-        <th>taxaImpressao</th>
-        <th>waterMark</th>
-      </tr>
+    <div class="container">
+      <h1>Listagem de Livros</h1>
+      <table class="table table-striped table-bordered table-hover">
 
-      <?php foreach ($livros as $livro) : ?>
-        <tr>
-          <td><?= $livro->nome ?></td>
-          <td><?= $livro->preco ?></td>
-          <td><?= $livro->descricao ?></td>
-          <td><?= $livro->isbn ?></td>
-          <td><?= $livro->tipoLivro ?></td>
-          <td><?= $livro->taxaImpressao ?></td>
-          <td><?= $livro->waterMark ?></td>
-        </tr>
-      <?php endforeach ?>
-    </table>
+          <tr>
+            <th>nome</th>
+            <th>preco</th>
+            <th>descricao</th>
+            <th>isbn</th>
+            <th>tipoLivro</th>
+            <th>taxaImpressao</th>
+            <th>waterMark</th>
+          </tr>
+
+        <?php foreach ($livros as $livro) : ?>
+          <tr>
+            <td><?= $livro->nome ?></td>
+            <td><?= $livro->preco ?></td>
+            <td><?= $livro->descricao ?></td>
+            <td><?= $livro->isbn ?></td>
+            <td><?= $livro->tipoLivro ?></td>
+            <td><?= $livro->taxaImpressao ?></td>
+            <td><?= $livro->waterMark ?></td>
+          </tr>
+        <?php endforeach ?>
+      </table>
+    </div>
 
   </body>
 </html>
