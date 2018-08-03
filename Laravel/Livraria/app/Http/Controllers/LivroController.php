4db5532dae8115568b64b01ccba58b205bc81532
DeiseClaudino <deise.claudino98@gmail.com>
2018-08-02 22:31:45 -0300

    Criando métodos que acessam o banco para buscar os dados, alterar e remover, as ações ainda não funcionam pois a classe Produto ainda não foi criada.

--- Laravel/Livraria/app/Http/Controllers/LivroController.php
+++ Laravel/Livraria/app/Http/Controllers/LivroController.php
@@ -12,4 +12,63 @@ class LivroController extends Controller {
      return view('listaLivros')->withLivros($livros);
   }
 
+  public function buscaProduto($id)
+    {
+        $lista =  DB::select("SELECT * FROM livros WHERE id = :id");
+        // $produto_buscado = $this->conexao->prepare($lista);
+        // $produto_buscado->bindValue(':id', $id);
+        // $produto_buscado->execute();
+        // foreach ($produto_buscado as $produto_buscado) {
+        //     $tipoLivro = $produto_buscado['tipoLivro'];
+        //     $factory = new LivroFactory();
+        //     $produto = $factory->criaPor($tipoLivro, $produto_buscado);
+        //     $produto->atualizaBaseadoEm($produto_buscado);
+        //     $produto->setId($produto_buscado['id']);
+        // }
+        // return $produto;
+        return view('alteraLivros')->withProduto($lista);
+    }
+
+    public function alteraProduto(Produto $produto)
+    {
+        // $isbn = "";
+        // if ($produto->temIsbn()) {
+        //     $isbn = $produto->getIsbn();
+        // }
+        // $waterMark = "";
+        // if ($produto->temWaterMark()) {
+        //     $waterMark = $produto->getWaterMark();
+        // }
+        // $taxaImpressao = "";
+        // if ($produto->temTaxaImpressao()) {
+        //     $taxaImpressao = $produto->getTaxaImpressao();
+        // }
+        // $tipoLivro = get_class($produto);
+        $query = DB::update("UPDATE livros SET
+        nome = :nome,
+        preco = :preco,
+        descricao = :descricao,
+        categoria_id = :categoria_id,
+        isbn = :isbn,
+        tipoLivro = :tipoLivro
+         WHERE id = :id");
+        $stmt = $this->conexao->prepare($query);
+        // $stmt->bindValue(':nome', $produto->getNome());
+        // $stmt->bindValue(':preco', $produto->getPreco());
+        // $stmt->bindValue(':descricao', $produto->getDescricao());
+        // $stmt->bindValue(':categoria_id', $produto->getCategoria()->getId());
+        // $stmt->bindValue(':isbn', $isbn);
+        // $stmt->bindValue(':tipoLivro', $tipoLivro);
+        // $stmt->bindValue(':id', $produto->getId());
+        return $stmt->execute();
+    }
+
+
+    public function removeProduto($id)
+    {
+       $query = DB::delete("DELETE FROM livros WHERE id = :id");
+       $stmt = $this->conexao->prepare($query);
+       $stmt->bindValue(':id', $id);
+       return $stmt->execute();
+    }
 }
