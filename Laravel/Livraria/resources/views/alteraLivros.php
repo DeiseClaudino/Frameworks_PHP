7120de0780a014acc4a9ba605c66ee3f95611046
DeiseClaudino <deise.claudino98@gmail.com>
2018-08-02 22:29:10 -0300

    Criando a view que mostra os dados de um determinado livro.

---
+++ Laravel/Livraria/resources/views/alteraLivros.php
@@ -0,0 +1,107 @@
+<html>
+  <head>
+      <link href="/css/app.css" rel="stylesheet">
+      <title>Controle de estoque</title>
+  </head>
+  <body>
+    <h1>Detalhes do produto: <?= $produto->nome ?> </h1>
+
+    <tr>
+        <td>Nome</td>
+        <td>
+
+             <input class="form-control" type="text" name="nome"
+                value="<?=$produto->getNome()?>">
+
+    </td>
+    </tr>
+    <tr>
+        <td>Preço</td>
+        <td>
+
+            <input  class="form-control" type="number" step="0.01" name="preco"
+                value="<?=$produto->getPreco()?>">
+        </td>
+
+    </tr>
+    <tr>
+        <td>Descrição</td>
+        <td>
+
+            <textarea class="form-control" name="descricao"><?=$produto->getDescricao()?></textarea>
+      </td>
+      </tr>
+      <tr>
+        <td>Categoria</td>
+
+     <td>
+          <select name="categoria_id" class="form-control">
+      			<?php
+
+              foreach ($categorias as $categoria) :
+
+                   $essaEhACategoria = $produto->getCategoria()->getId() == $categoria->getId();
+
+                   $selecao = $essaEhACategoria ? "selected='selected'" : "";
+               ?>
+            <option value="<?=$categoria->getId()?>" <?=$selecao?>>
+              <?=$categoria->getNome()?>
+            </option>
+          <?php
+                endforeach
+                ?>
+      		</select>
+        </td>
+
+    </tr>
+    <tr>
+        <td>Tipo do produto</td>
+        <td>
+           <select name="tipoProduto" class="form-control">
+             <optgroup label="Livros">
+               <?php
+                $tipos = array("Livro Fisico", "Ebook");
+                foreach ($tipos as $tipo) :
+                  $tipoSemEspaco = str_replace(" ", "", $tipo);
+                    $esseEhOTipo = get_class($produto) == $tipoSemEspaco;
+                    $selecao = $esseEhOTipo ? "selected='selected'" : "";
+                ?>
+                    <option value="<?=$tipoSemEspaco?>" <?=$selecao?>>
+                        <?=$tipo?>
+                    </option>
+
+                <?php
+                endforeach
+                ?>
+             </optgroup>
+            </select>
+       </td>
+    </tr>
+    <tr>
+        <td>ISBN (caso seja um Livro)</td>
+        <td>
+            <input type="text" name="isbn" class="form-control"
+                        value="<?php if ($produto->temIsbn()) {
+                    echo $produto->getIsbn();
+                };?>" >
+
+      </td>
+    </tr>
+    <tr>
+      <td>Taxa de Impressão (caso seja um Livro Físico)</td>
+        <td>
+          <input type="text" class="form-control" name="taxaImpressao" value="<?php if ($produto->temTaxaImpressao()) {);
+                      echo $produto->getTaxaImpressao();
+                  } ?>">
+        </td>
+
+    </tr>
+    <tr>
+        <td>WaterMark (caso seja um Ebook)</td>
+        <td>
+         <input type="text" class="form-control" name="WaterMark" value="<?php if ($produto->temWaterMark()) {);
+                      echo $produto->getWaterMark();
+                  } ?>">
+        </td>
+      </tr>
+  </body>
