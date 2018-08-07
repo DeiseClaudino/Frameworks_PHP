f0770ec36c4e8525d2cee14ebba4a644b815bc2f
DeiseClaudino <deise.claudino98@gmail.com>
2018-08-06 22:28:34 -0300

    Criando a classe categoria.

---
+++ Laravel/Livraria/app/Categoria.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    private $id;
+    private $nome;
+    public function getId()
+    {
+      return $this->id;
+    }
+    public function setId($id)
+    {
+      return $this->id = $id;
+    }
+    public function getNome()
+    {
+      return $this->nome;
+    }
+    public function setNome($nome)
+    {
+      return $this->nome = $nome;
+    }
+}
