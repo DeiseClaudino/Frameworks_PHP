c46bd8f98854bdf2e46484de3b64ce33f38a1561
DeiseClaudino <deise.claudino98@gmail.com>
2018-08-08 23:08:26 -0300

    Criando rota para a view responsavél por adicionar novos livros.

--- Laravel/Livraria/routes/web.php
+++ Laravel/Livraria/routes/web.php
@@ -7,3 +7,5 @@ Route::get('/listaLivros', 'LivroController@lista');
 Route::get('/listaLivros/alterar/{id}', 'LivroController@buscaProduto')->where('id', '[0-9]+');
 
 Route::get('/listaLivros/remove/{id}', 'LivroController@removeProduto')->where('id', '[0-9]+');
+
+Route::get('/adicionarNovo', 'LivroController@adicionaProduto')->where('id', '[0-9]+');
