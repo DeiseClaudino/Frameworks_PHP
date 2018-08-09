3b469f46d5b563416f93cfc0ffd46cb7c454216b
DeiseClaudino <deise.claudino98@gmail.com>
2018-08-08 22:26:19 -0300

    Incluindo rota de página já existente no menu.

--- Laravel/Livraria/resources/views/cabecalho.blade.php
+++ Laravel/Livraria/resources/views/cabecalho.blade.php
@@ -13,7 +13,7 @@
             <a class="nav-link " href="/" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Bootstrap');">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Docs');">Lista</a>
+            <a class="nav-link" href="/listaLivros" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Docs');">Lista</a>
           </li>
           <li class="nav-item">
             <a class="nav-link " href="#" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Examples');">Inserir +</a>
