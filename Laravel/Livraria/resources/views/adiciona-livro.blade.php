a601474d02b9ca0616c76b2dd995c8f8d0f05a0a
DeiseClaudino <deise.claudino98@gmail.com>
2018-08-08 23:09:02 -0300

    Criando a view responsavél por adicionar novos livros.

---
+++ Laravel/Livraria/resources/views/adiciona-livro.blade.php
@@ -0,0 +1,13 @@
+@extends('produto-formulario-base')
+@section('outros')
+      <h1>Adicionar Livros</h1>
+
+    <tr>
+      <td>
+        <button class="btn btn-primary" type="submit">Cadastrar</button>
+      </td>
+    </tr>
+@stop
+@extends('rodape')
+@section('rodape')
+@stop
