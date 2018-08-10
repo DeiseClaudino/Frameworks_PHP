8bab19b562e4f65e4066faf46f88993115f25ca2
DeiseClaudino <deise.claudino98@gmail.com>
2018-08-09 22:11:11 -0300

    Chamando o método post para ol formulário .

--- Laravel/Livraria/resources/views/adiciona-livro.blade.php
+++ Laravel/Livraria/resources/views/adiciona-livro.blade.php
@@ -1,13 +1,14 @@
 @extends('produto-formulario-base')
 @section('outros')
       <h1>Adicionar Livros</h1>
-
-    <tr>
-      <td>
-        <button class="btn btn-primary" type="submit">Cadastrar</button>
-      </td>
-    </tr>
-@stop
+<form action="/adicionarNovo" method="post">
+      <tr>
+        <td>
+          <button class="btn btn-primary" type="submit">Cadastrar</button>
+        </td>
+      </tr>
+  @stop
+</form>
 @extends('rodape')
 @section('rodape')
 @stop
