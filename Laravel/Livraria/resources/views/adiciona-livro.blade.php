a9008549699eed83a28f884e038c6d3205c58d65
DeiseClaudino <deise.claudino98@gmail.com>
2018-08-09 23:08:10 -0300

    Identando código.

--- Laravel/Livraria/resources/views/adiciona-livro.blade.php
+++ Laravel/Livraria/resources/views/adiciona-livro.blade.php
@@ -1,14 +1,14 @@
 @extends('produto-formulario-base')
 @section('outros')
       <h1>Adicionar Livros</h1>
-<form action="/adicionarNovo" method="post">
-      <tr>
-        <td>
-          <button class="btn btn-primary" type="submit">Cadastrar</button>
-        </td>
-      </tr>
-  @stop
-</form>
+      <form action="/adicionarNovo" method="post">
+            <tr>
+              <td>
+                <button class="btn btn-primary" type="submit">Cadastrar</button>
+              </td>
+            </tr>
+        @stop
+      </form>
 @extends('rodape')
 @section('rodape')
 @stop
