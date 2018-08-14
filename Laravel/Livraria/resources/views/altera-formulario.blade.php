345b7ff4d2e07b23a26835506d1e9e598fe50437
DeiseClaudino <deise.claudino98@gmail.com>
2018-08-13 22:26:30 -0300

    Criando formulário que busca os dados para a tela de alteração.

---
+++ Laravel/Livraria/resources/views/altera-formulario.blade.php
@@ -0,0 +1,80 @@
+@extends('cabecalho')
+@section('conteudo')
+  <table class="table table-striped table-bordered table-hover">
+          <tr>
+              <th>Nome</th>
+              <td>
+
+                   <input class="form-control" type="text" name="nome"
+                      value="{{$produto->nome}}">
+
+              </td>
+          </tr>
+          <tr>
+              <th>Preço</th>
+              <td>
+
+                  <input  class="form-control" type="number" step="0.01" name="preco"
+                      value="{{$produto->preco}}">
+              </td>
+
+          </tr>
+          <tr>
+              <th>Descrição</th>
+                <td>
+
+                  <textarea class="form-control" name="descricao">{{$produto->descricao}}</textarea>
+                </td>
+            </tr>
+            <tr>
+              <th>Categoria</th>
+
+              <td>
+
+            </td>
+          </tr>
+          <tr>
+              <th>Tipo do produto</th>
+              <td>
+                 <select name="tipoProduto" class="form-control">
+                   <optgroup label="Livros">
+                     <?php
+                      $tipos = array("Livro Fisico", "Ebook");
+                      foreach ($tipos as $tipo) :
+                        $tipoSemEspaco = str_replace(" ", "", $tipo);
+                          $esseEhOTipo = get_class($produto) == $tipoSemEspaco;
+                          $selecao = $esseEhOTipo ? "selected='selected'" : "";
+                      ?>
+                          <option value="<?=$tipoSemEspaco?>" <?=$selecao?>>
+                              <?=$tipo?>
+                          </option>
+
+                      <?php
+                      endforeach
+                      ?>
+                   </optgroup>
+                  </select>
+             </td>
+          </tr>
+          <tr>
+              <th>ISBN (caso seja um Livro)</th>
+              <td>
+
+            </td>
+          <tr>
+          </tr>
+            <th>Taxa de Impressão (caso seja um Livro Físico)</th>
+              <td>
+
+              </td>
+
+          </tr>
+          <tr>
+              <th>WaterMark (caso seja um Ebook)</th>
+              <td>
+
+              </td>
+            </tr>
+            @yield('outros')
+
+  @stop
