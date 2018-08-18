86039e563286b8384fcc757be7ea4a31a1dfd9fa
DeiseClaudino <deise.claudino98@gmail.com>
2018-08-17 23:27:17 -0300

    Reescrevendo o método de listagem de categorias  utilizando o Eloquent ORM.

--- Laravel/Livraria/app/Http/Controllers/CategoriaController.php
+++ Laravel/Livraria/app/Http/Controllers/CategoriaController.php
@@ -8,18 +8,7 @@ class CategoriaController extends Controller
 {
    public function listaCategorias()
    {
-       $categorias = array();
-       $resultado= DB::select(
-         "SELECT * FROM categorias",
-         PDO::FETCH_ASSOC
-       );
-       foreach ($resultado as $linha) {
-           $categoria = new Categoria();
-           $categoria->setId($linha['id']);
-           $categoria->setNome($linha['nome']);
-           array_push($categorias, $categoria);
-       }
-       return view('alteraLivros')->withCategorias($categorias);
-
+     $categorias = Categoria::all();
+     return view('alteraLivros')->withCategorias($categorias);
   }
 }
