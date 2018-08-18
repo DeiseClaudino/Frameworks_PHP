f39b2f95f0c4d93cb8d75e0752f7a86664663e8e
DeiseClaudino <deise.claudino98@gmail.com>
2018-08-17 22:25:00 -0300

    Incluindo informações requeridas na inserção de um novo livro.

--- Laravel/Livraria/app/Livro.php
+++ Laravel/Livraria/app/Livro.php
@@ -1,10 +1,13 @@
 <?php
 
-namespace Livro;
+namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Livro extends Model
 {
+    public $timestamps = false;
+    protected $fillable = array('nome', 'preco', 'descricao', 'categoria_id',
+      'isbn', 'tipoLivro', 'taxaImpressao', 'waterMark');
 
 }
