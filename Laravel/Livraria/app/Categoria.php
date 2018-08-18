d94114eca83299cbbf44fe3f50ae736b56b36670
DeiseClaudino <deise.claudino98@gmail.com>
2018-08-17 23:26:36 -0300

    Reescrevendo o método de listagem de categorias  utilizando o Eloquent ORM.

--- Laravel/Livraria/app/Categoria.php
+++ Laravel/Livraria/app/Categoria.php
@@ -6,22 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
-    private $id;
-    private $nome;
-    public function getId()
-    {
-      return $this->id;
-    }
-    public function setId($id)
-    {
-      return $this->id = $id;
-    }
-    public function getNome()
-    {
-      return $this->nome;
-    }
-    public function setNome($nome)
-    {
-      return $this->nome = $nome;
-    }
+    public $timestamps = false;
 }
