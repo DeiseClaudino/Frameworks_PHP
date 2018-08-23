fd4351b3a76bce0c0d0683cfaa6590d2dbbc8b0d
DeiseClaudino <deise.claudino98@gmail.com>
2018-08-22 22:59:13 -0300

    Adicionando categoria no modelo que manipula os dados que serão inseridos

--- Laravel/Livraria/app/Livro.php
+++ Laravel/Livraria/app/Livro.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Livro extends Model
 {
     public $timestamps = false;
-    protected $fillable = array('nome', 'preco', 'descricao', 'categoria_id',
+    protected $fillable = array('nome', 'preco', 'descricao', 'categoria',
       'isbn', 'tipoLivro', 'taxaImpressao', 'waterMark');
 
 }
