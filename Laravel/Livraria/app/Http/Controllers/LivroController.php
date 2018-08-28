231636c6040c6fbd7fae947c846a0d60d0fc99b5
DeiseClaudino <deise.claudino98@gmail.com>
2018-08-27 23:02:08 -0300

    Passando middleware que bloqueia aceeso aos métodos de adição e remoção, caso o usuário não esteja logado.

--- Laravel/Livraria/app/Http/Controllers/LivroController.php
+++ Laravel/Livraria/app/Http/Controllers/LivroController.php
@@ -8,6 +8,12 @@ use Request;
 
 class LivroController extends Controller {
 
+  public function __construct()
+{
+    $this->middleware('auth',
+        ['only' => ['adicionaProduto', 'removeProduto']]);
+}
+
   public function lista()
   {
     $produto = Livro::all();
