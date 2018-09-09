8936a48258be90ddd2bd50a31b2a384a3df29e20
DeiseClaudino <deise.claudino98@gmail.com>
2018-09-09 14:09:55 -0300

    Criando o request que verifica os valores do formulário.

---
+++ Laravel/Livraria/app/Http/Requests/LivrosRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LivrosRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+          'nome' => 'required|max:100',
+          'preco' => 'required|numeric',
+          'descricao' => 'required|max:255',
+          'categoria',
+          'tipoProduto',
+          'ISBN' => 'required|max:255',
+          'taxaImpressao' => 'required|max:255',
+          'waterMark' => 'required|max:255',
+
+
+        ];
+    }
+}
