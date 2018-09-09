876a5c440d782a0aa67e3c9d930ea0be6683b97f
DeiseClaudino <deise.claudino98@gmail.com>
2018-09-09 14:05:13 -0300

    Excluindo a validação do id passado pela rota.

--- Laravel/Livraria/routes/web.php
+++ Laravel/Livraria/routes/web.php
@@ -4,7 +4,7 @@ Route::get('/listaLivros', 'LivroController@lista');
 
 Route::get('/listaLivros/alterar/{id}', 'LivroController@buscaProduto')->where('id', '[0-9]+');
 
-Route::get('/listaLivros/remove/{id}', 'LivroController@removeProduto')->where('id', '[0-9]+');
+Route::get('/listaLivros/remove/{id}', 'LivroController@removeProduto');
 
 Route::get('/formulario', 'LivroController@abreFormulario');
 
