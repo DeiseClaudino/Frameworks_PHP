ee5d27aef9b918054d9d80a23dc2bd41af6628f9
DeiseClaudino <deise.claudino98@gmail.com>
2018-09-21 21:42:09 -0300

    Transformando o botão em âncora

--- Laravel/Livraria/resources/views/alteraLivros.blade.php
+++ Laravel/Livraria/resources/views/alteraLivros.blade.php
@@ -1,15 +1,16 @@
 @extends('altera-formulario')
 @section('outros')
   <h1>Alterando: {{$produto->nome}} </h1>
-
+  <form method="post>
 
       <tr>
         <td>
-          <button type="submit" class="btn btn-primary">Alterar</button>
+          <a class="btn btn-primary" href="/listaLivros/alterado">Alterar</a>
         </td>
       </tr>
   </table>
-@stop
+  @stop
+</form>
 @extends('rodape')
 @section('rodape')
 @stop
