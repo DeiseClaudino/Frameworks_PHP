1b1d17dea60a6d65536cd86b707e02a989000669
DeiseClaudino <deise.claudino98@gmail.com>
2018-09-22 18:28:04 -0300

    Removendo o chamado do formulário.

--- Laravel/Livraria/resources/views/adiciona-livro.blade.php
+++ Laravel/Livraria/resources/views/adiciona-livro.blade.php
@@ -1,14 +1,12 @@
 @extends('produto-formulario-base')
 @section('outros')
       <h1>Adicionar Livros</h1>
-      <form action="/adicionarnovo" method="get">
             <tr>
               <td>
                 <button type="submit" class="btn btn-primary">Adicionar</button>
               </td>
             </tr>
         @stop
-      </form>
 @extends('rodape')
 @section('rodape')
 @stop
