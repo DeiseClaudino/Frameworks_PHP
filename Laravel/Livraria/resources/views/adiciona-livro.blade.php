a7b8656661f8e04d6bd7cb81bd8d5f12952f9771
DeiseClaudino <deise.claudino98@gmail.com>
2018-09-21 21:38:49 -0300

    Enviando o formulario via get.

--- Laravel/Livraria/resources/views/adiciona-livro.blade.php
+++ Laravel/Livraria/resources/views/adiciona-livro.blade.php
@@ -1,7 +1,7 @@
 @extends('produto-formulario-base')
 @section('outros')
       <h1>Adicionar Livros</h1>
-      <form method="post" action="/adicionarNovo">
+      <form action="/adicionarnovo" method="get">
             <tr>
               <td>
                 <button type="submit" class="btn btn-primary">Adicionar</button>
