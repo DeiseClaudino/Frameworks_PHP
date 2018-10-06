f14ef08e5866e15c3f8696f9c1fe9421bfd6ab1c
DeiseClaudino <deise.claudino98@gmail.com>
2018-10-05 22:26:03 -0300

    Forçando que o id passado seja um int.

--- Laravel/Livraria/routes/web.php
+++ Laravel/Livraria/routes/web.php
@@ -4,7 +4,7 @@ Route::get('/listaLivros', 'LivroController@lista');
 
 Route::get('/listaLivros/alterar/{id}', 'LivroController@buscaProduto')->where('id', '[0-9]+');
 
-Route::get('/listaLivros/alterado/{id}', 'LivroController@alteraProduto');
+Route::get('/listaLivros/alterado/{id}', 'LivroController@alteraProduto')->where('id', '[0-9]+');
 
 Route::get('/listaLivros/remove/{id}', 'LivroController@removeProduto');
 
