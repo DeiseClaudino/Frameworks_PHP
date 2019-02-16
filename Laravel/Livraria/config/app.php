a180350ae0734c18b0aefa457d8b0ed6cade425d
DeiseClaudino <deise.claudino98@gmail.com>
2019-02-16 14:39:16 -0200

    Instancia provedor do mongoDB

--- Laravel/Livraria/config/app.php
+++ Laravel/Livraria/config/app.php
@@ -146,8 +146,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        // Jenssegers\Mongodb\MongodbServiceProvider::class,
-
+        Jenssegers\Mongodb\MongodbServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -209,7 +208,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        
     ],
 
 ];
